a8eb3812b0c5a80e2ffdc3722be208b74994ed11
acharaakshit <acharaakshit@gmail.com>
2020-05-27 22:22:50 +0530

    1. decision variables can be assigned expressions. 2. Set decision variables can only be of type enums or integers.

--- R/variable.R
+++ R/variable.R
@@ -57,6 +57,7 @@ variable <- R6Class("variable",
                                 assert(!test_null(value))
                               if(test_choice(self$type, .globals$types$single)){
                                 assert_scalar(value)
+                                assert_null(sub_type)
                                 if(test_choice(self$type, "int")){
                                   assert(all(value == floor(value)))
                                 }else if(test_choice(self$type, "float")){
@@ -70,17 +71,16 @@ variable <- R6Class("variable",
                               if(test_choice(type, .globals$types$collection)){
                                 if(test_choice(self$type , "enum")){
                                   assert(test_character(value),
-                                         !test_scalar(value), combine = "and")
+                                         !test_scalar(value), test_null(sub_type), combine = "and")
                                 }else if(test_choice(self$type , "set")){
-                                  
-                                  assert(!test_null(sub_type), test_choice(sub_type, .globals$sub_types),
-                                         test_atomic_vector(value), combine = "and")
+                                  assert(test_choice(sub_type, .globals$sub_types),
+                                         !test_choice(sub_type , "set"), combine = "and")
                                   assert(!test_scalar(value), test_choice(typeof(value), "double"),
                                          test_atomic_vector(value), combine = "and")
                                   self$sub_type = sub_type
                                 }else{
-                                  assert(!test_null(sub_type), test_choice(sub_type, .globals$sub_types),
-                                         combine = "and")
+                                  assert(test_choice(sub_type, .globals$sub_types),
+                                         !test_choice(sub_type, "array"), combine = "and")
                                   assert(test_array(value) , test_true(length(dim(value)) < 6), 
                                          combine = "and")
                                   if(test_choice(sub_type, "enum")){
@@ -97,8 +97,26 @@ variable <- R6Class("variable",
                               
                               if(test_choice(kind, "decision")){
                                 if(!test_null(domain)){
-                                self$domain = domain
+                                  self$domain = domain
                                 }
+                                if(test_choice(type, .globals$types$collection) &&
+                                   !test_choice(type, "enum")){
+                                  if(test_choice(type,"set")){
+                                    assert_choice(sub_type, c("int","enum"))
+                                  }else{
+                                    assert(test_choice(sub_type, .globals$sub_types),
+                                           !test_choice(sub_type, "array"), combine = "and")
+                                  }
+                                  self$sub_type = sub_type
+                                }
+                                
+                                assert_null(value)
+                                if(!test_null(expr)){
+                                  assert_r6(expr, "get_expression")
+                                  self$expr = expr;
+                                }
+                                
+                                
                               }
                               
                               if(!is.null(expr)){
