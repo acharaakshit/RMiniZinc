069a14dea1def465ee8add3d67b90c87921b7284
acharaakshit <acharaakshit@gmail.com>
2020-06-12 22:31:14 +0530

    creating a vector of the assignment values

--- src/sol_parse.cpp
+++ src/sol_parse.cpp
@@ -1,10 +1,10 @@
 #include <Rcpp.h>
 #include <minizinc/parser.hh>
 
-
 using namespace Rcpp;
-using namespace MiniZinc;
 using namespace std;
+using namespace MiniZinc;
+
 
 //' @title parse the solution
 //' 
@@ -14,10 +14,11 @@ using namespace std;
 //' @export sol_parse
 //' @useDynLib rminizinc, .registration=TRUE
 //' @param solutionString solution of the model as a string representation
+//' 
 // [[Rcpp::export]]
 void sol_parse(std::string solutionString) {
   Env* env = new Env();
-  vector<string> ip = {};
+  std::vector<std::string> ip = {};
   ostream& os = cerr;
   vector<SyntaxError> se;
   Model *model = MiniZinc::parseFromString(*env, solutionString, "",  ip, true, true, true, os, se);
@@ -32,24 +33,25 @@ void sol_parse(std::string solutionString) {
       int type = items[i]->cast<AssignI>()->e()->eid();
       switch(type){
       case Expression::E_INTLIT:
-        //retval.push_back(items[i]->cast<AssignI>()->e()->cast<IntLit>()->v());
+        retval.push_back(items[i]->cast<AssignI>()->e()->cast<IntLit>()->v().toInt());
         break;
       case Expression::E_FLOATLIT:
-        //retval.push_back(items[i]->cast<AssignI>()->e()->cast<FloatLit>()->v());
+        retval.push_back(items[i]->cast<AssignI>()->e()->cast<FloatLit>()->v().toDouble());
         break;
       case Expression::E_BOOLLIT:
-        //retval.push_back(items[i]->cast<AssignI>()->e()->cast<BoolLit>()->v());
+        retval.push_back(items[i]->cast<AssignI>()->e()->cast<BoolLit>()->v());
         break;
       case Expression::E_SETLIT:
         if(items[i]->cast<AssignI>()->e()->cast<SetLit>()->isv()!=NULL){
           // integer set
-          //int max_val =  items[i]->cast<AssignI>()->e()->cast<SetLit>()->isv()->max().toInt();
-          //int min_val = items[i]->cast<AssignI>()->e()->cast<SetLit>()->isv()->min().toInt();  
+          int max_val =  items[i]->cast<AssignI>()->e()->cast<SetLit>()->isv()->max().toInt();
+          int min_val = items[i]->cast<AssignI>()->e()->cast<SetLit>()->isv()->min().toInt();  
           //retval.push_back(c(max_val,min_val));
+          NumericVector snvec = {(double)max_val, (double)min_val}; 
         }else{
           // floating point set
-          //float max_val =  items[i]->cast<AssignI>()->e()->cast<SetLit>()->fsv()->max().toDouble();
-          //float min_val = items[i]->cast<AssignI>()->e()->cast<SetLit>()->fsv()->min().toDouble();
+          float max_val =  items[i]->cast<AssignI>()->e()->cast<SetLit>()->fsv()->max().toDouble();
+          float min_val = items[i]->cast<AssignI>()->e()->cast<SetLit>()->fsv()->min().toDouble();
           //retval.push_back(c(max_val,min_val));
         }
         
